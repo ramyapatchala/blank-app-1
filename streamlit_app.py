3fb71e54ace9e152dedf386e68918b05a4c6f89f
ramyapatchala <rpatchal@syr.edu>
2024-09-19 17:23:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,54 @@
 import streamlit as st
+from openai import OpenAI
+import os
+from PyPDF2 import PdfReader
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+import chromadb
+
+if 'openai_client' not in st.session_state:
+    api_key = st.secrets['key1']
+    st.session_state.openai_client = OpenAI(api_key = api_key)
+
+def coll_function():
+    client = chromadb.PersistentClient()
+    collection = client.get_or_create_collection("L4_Collection")
+    datafiles_path = os.path.join(os.getcwd(), "datafiles")
+    pdf_files = [f for f in os.listdir(datafiles_path) if f.endswith('.pdf')]
+        
+    for pdf_file in pdf_files:
+        file_path = os.path.join(datafiles_path, pdf_file)
+        with open(file_path, 'rb') as file:
+            pdf_reader = PdfReader(file)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+            openai_client = st.session_state.openai_client
+            response = openai_client.embeddings.create(
+                        input=text,
+                        model="text-embedding-3-small")
+            embedding = response.data[0].embedding
+            collection.add(
+                documents=[text],
+                ids=[pdffile],
+                embeddings=[embedding]
+            )
+    user_input = st.text_input("Enter some text:")
+    embedding = openai_client.embeddings.create(
+                        input=user_input,
+                        model="text-embedding-3-small")
+    results = client.query(query_embeddings = embedding,
+                            n_results= 3)
+    for i in range(len(results['ids'][0])):
+        doc_id = results['ids'][0][i]
+        st.write(f"The following file/syllabus might be helpful: {doc_id}")
+
+coll_function()
+
+    
+    
+
+    
